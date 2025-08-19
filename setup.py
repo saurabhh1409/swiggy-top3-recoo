f8e968dd22e2e170e8ed89a27ca4a243c40531d2
saurabhh1409 <saurabhnishad1409@gmail.com>
2025-08-19 16:26:35 +0530

    cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project recommend top 3 food item on swiggy home page',
+    author='saurabh',
+    license='',
+)
